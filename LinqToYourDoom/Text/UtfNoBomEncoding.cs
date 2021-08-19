e8dec3832c9a642d37f715de4ceb39f243580dbe
Odepax <odepaxforuma@gmail.com>
2021-08-19 17:52:54 +0100

    Added UTF string/bytes conversion extensions

---
+++ LinqToYourDoom/Text/UtfNoBomEncoding.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace LinqToYourDoom {
+	public static class UtfNoBomEncoding {
+		public static readonly UTF8Encoding Utf8NoBom = new(/* byteOrderMark */ false);
+		public static readonly UnicodeEncoding Utf16NoBom = new(bigEndian: false, byteOrderMark: false);
+		public static readonly UTF32Encoding Utf32NoBom = new(bigEndian: false, byteOrderMark: false);
+	}
+}
