947f6895d12b8c2d369ad52d751443d684b7a62f
Odepax <odepaxforuma@gmail.com>
2022-01-01 15:20:14 +0000

    Revamped Disposable extensions for collections

--- LinqToYourDoom.Tests/System/Extensions/DisposableExtensionsTests.cs
+++ LinqToYourDoom.Tests/System/Extensions/DisposableExtensionsTests.cs
@@ -10,23 +10,416 @@ sealed class DummyDisposable : IDisposable {
 }
 
 static class DisposableExtensionsTests {
+	// IEnumerable<T> where T : IDisposable
+	// ----
+
 	[Test]
-	public static void DisposeAll() {
-		new List<DummyDisposable>(3) {
+	public static void IEnumerable_TDisposable_DisposeAll() {
+		new[] {
+			new DummyDisposable().Tee(out var a),
+			new DummyDisposable().Tee(out var b),
+			new DummyDisposable().Tee(out var c)
+		}
+			.DisposeAll();
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	[Test]
+	public static void IEnumerable_TDisposable_DisposeAll_predicate() {
+		new[] {
+			new DummyDisposable().Tee(out var a),
+			new DummyDisposable().Tee(out var b),
+			new DummyDisposable().Tee(out var c)
+		}
+			.DisposeAll(it => it != b);
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	// IReadOnlyDictionary<TKey, TValue> where TValue : IDisposable
+	// ----
+
+	[Test]
+	public static void IReadOnlyDictionary_TDisposable_DisposeAll() {
+		new Dictionary<char, DummyDisposable> {
+			['A'] = new DummyDisposable().Tee(out var a),
+			['B'] = new DummyDisposable().Tee(out var b),
+			['C'] = new DummyDisposable().Tee(out var c)
+		}
+			.DisposeAll();
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	[Test]
+	public static void IReadOnlyDictionary_TDisposable_DisposeAll_predicate_1() {
+		new Dictionary<char, DummyDisposable> {
+			['A'] = new DummyDisposable().Tee(out var a),
+			['B'] = new DummyDisposable().Tee(out var b),
+			['C'] = new DummyDisposable().Tee(out var c)
+		}
+			.DisposeAll(value => value != b);
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	[Test]
+	public static void IReadOnlyDictionary_TDisposable_DisposeAll_predicate_2() {
+		new Dictionary<char, DummyDisposable> {
+			['A'] = new DummyDisposable().Tee(out var a),
+			['B'] = new DummyDisposable().Tee(out var b),
+			['C'] = new DummyDisposable().Tee(out var c)
+		}
+			.DisposeAll((key, value) => key != 'B');
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	// IEnumerable<T>
+	// ----
+
+	[Test]
+	public static void IEnumerable_T_DisposeAll_selector() {
+		new[] {
+			('A', new DummyDisposable().Tee(out var a)),
+			('B', new DummyDisposable().Tee(out var b)),
+			('C', new DummyDisposable().Tee(out var c))
+		}
+			.DisposeAll(it => it.Item2);
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	[Test]
+	public static void IEnumerable_T_DisposeAll_trySelector_1() {
+		new[] {
+			('A', new DummyDisposable().Tee(out var a)),
+			('B', new DummyDisposable().Tee(out var b)),
+			('C', new DummyDisposable().Tee(out var c))
+		}
+			.DisposeAll(TryFilter);
+
+		static bool TryFilter((char, DummyDisposable) it, out IDisposable @out) {
+			@out = it.Item2;
+			return it.Item1 != 'B';
+		}
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	[Test]
+	public static void IEnumerable_T_DisposeAll_trySelector_2() {
+		new[] {
+			('A', new DummyDisposable().Tee(out var a)),
+			('B', new DummyDisposable().Tee(out var b)),
+			('C', new DummyDisposable().Tee(out var c))
+		}
+			.DisposeAll(it => (it.Item1 != 'B', it.Item2));
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	// IReadOnlyDictionary<TKey, TValue>
+	// ----
+
+	[Test]
+	public static void IReadOnlyDictionary_T_DisposeAll_selector() {
+		new Dictionary<char, (int, DummyDisposable)> {
+			['A'] = (1, new DummyDisposable().Tee(out var a)),
+			['B'] = (2, new DummyDisposable().Tee(out var b)),
+			['C'] = (3, new DummyDisposable().Tee(out var c))
+		}
+			.DisposeAll((key, value) => value.Item2);
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	[Test]
+	public static void IReadOnlyDictionary_T_DisposeAll_trySelector_1() {
+		new Dictionary<char, (int, DummyDisposable)> {
+			['A'] = (1, new DummyDisposable().Tee(out var a)),
+			['B'] = (2, new DummyDisposable().Tee(out var b)),
+			['C'] = (3, new DummyDisposable().Tee(out var c))
+		}
+			.DisposeAll(TryFilter);
+
+		static bool TryFilter(char key, (int, DummyDisposable) value, out IDisposable @out) {
+			@out = value.Item2;
+			return key!= 'B';
+		}
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	[Test]
+	public static void IReadOnlyDictionary_T_DisposeAll_trySelector_2() {
+		new Dictionary<char, (int, DummyDisposable)> {
+			['A'] = (1, new DummyDisposable().Tee(out var a)),
+			['B'] = (2, new DummyDisposable().Tee(out var b)),
+			['C'] = (3, new DummyDisposable().Tee(out var c))
+		}
+			.DisposeAll((key, value) => (key != 'B', value.Item2));
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+	}
+
+	// ICollection<T> where T : IDisposable
+	// ----
+
+	[Test]
+	public static void ICollection_TDisposable_RemoveAndDispose() {
+		new List<DummyDisposable> {
 			new DummyDisposable().Tee(out var a),
 			new DummyDisposable().Tee(out var b),
 			new DummyDisposable().Tee(out var c)
 		}
 			.Tee(out var list)
-			.DisposeAll()
-			.Clear();
+			.RemoveAndDispose(b);
 
-		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(a.IsDisposed);
 		Assert.IsTrue(b.IsDisposed);
-		Assert.IsTrue(c.IsDisposed);
+		Assert.IsFalse(c.IsDisposed);
 
-		Assert.IsEmpty(list);
+		Assert.AreEqual(new[] { a, c }, list);
 	}
+	
+	// IList<T> where T : IDisposable
+	// ----
+
+	[Test]
+	public static void IList_TDisposable_RemoveAtAndDispose() {
+		new List<DummyDisposable> {
+			new DummyDisposable().Tee(out var a),
+			new DummyDisposable().Tee(out var b),
+			new DummyDisposable().Tee(out var c)
+		}
+			.Tee(out var list)
+			.RemoveAtAndDispose(1);
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsFalse(c.IsDisposed);
+
+		Assert.AreEqual(new[] { a, c }, list);
+	}
+
+	[Test]
+	public static void IList_TDisposable_SetAndDispose() {
+		new List<DummyDisposable> {
+			new DummyDisposable().Tee(out var a),
+			new DummyDisposable().Tee(out var b),
+			new DummyDisposable().Tee(out var c)
+		}
+			.Tee(out var list)
+			.SetAndDispose(1, new DummyDisposable().Tee(out var d));
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsFalse(c.IsDisposed);
+		Assert.IsFalse(d.IsDisposed);
+
+		Assert.AreEqual(new[] { a, d, c }, list);
+	}
+
+	// IDictionary<TKey, TValue> where TValue : IDisposable
+	// ----
+
+	[Test]
+	public static void IDictionary_TDisposable_RemoveAndDispose() {
+		new Dictionary<char, DummyDisposable> {
+			['A'] = new DummyDisposable().Tee(out var a),
+			['B'] = new DummyDisposable().Tee(out var b),
+			['C'] = new DummyDisposable().Tee(out var c)
+		}
+			.Tee(out var dictionary)
+			.RemoveAndDispose('B');
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsFalse(c.IsDisposed);
+
+		Assert.AreEqual(new[] { KeyValuePair.Create('A', a), KeyValuePair.Create('C', c) }, dictionary);
+	}
+
+	[Test]
+	public static void IDictionary_TDisposable_RemoveAndDispose_non_existent() {
+		new Dictionary<char, DummyDisposable>()
+			.Tee(out var dictionary)
+			.RemoveAndDispose('X');
+
+		Assert.IsEmpty(dictionary);
+	}
+
+	[Test]
+	public static void IDictionary_TDisposable_SetAndDispose() {
+		new Dictionary<char, DummyDisposable> {
+			['A'] = new DummyDisposable().Tee(out var a),
+			['B'] = new DummyDisposable().Tee(out var b),
+			['C'] = new DummyDisposable().Tee(out var c)
+		}
+			.Tee(out var dictionary)
+			.SetAndDispose('B', new DummyDisposable().Tee(out var d));
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsFalse(c.IsDisposed);
+		Assert.IsFalse(d.IsDisposed);
+
+		Assert.AreEqual(new[] { KeyValuePair.Create('A', a), KeyValuePair.Create('B', d), KeyValuePair.Create('C', c) }, dictionary);
+	}
+
+	[Test]
+	public static void IDictionary_TDisposable_SetAndDispose_non_existent() {
+		new Dictionary<char, DummyDisposable>()
+			.Tee(out var dictionary)
+			.SetAndDispose('X', new DummyDisposable().Tee(out var x));
+
+		Assert.IsFalse(x.IsDisposed);
+
+		Assert.AreEqual(new[] { KeyValuePair.Create('X', x) }, dictionary);
+	}
+
+	// ICollection<T>
+	// ----
+
+	[Test]
+	public static void ICollection_T_RemoveAndTryDispose() {
+		new List<(int, DummyDisposable)> {
+			(1, new DummyDisposable().Tee(out var a)),
+			(2, new DummyDisposable().Tee(out var b)),
+			(3, new DummyDisposable().Tee(out var c))
+		}
+			.Tee(out var list)
+			.RemoveAndTryDispose((2, b));
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed); // b is disposable, but the tuple is not!
+		Assert.IsFalse(c.IsDisposed);
+
+		Assert.AreEqual(new[] { (1, a), (3, c) }, list);
+	}
+
+	// IList<T>
+	// ----
+
+	[Test]
+	public static void IList_T_RemoveAtAndTryDispose() {
+		new List<(int, DummyDisposable)> {
+			(1, new DummyDisposable().Tee(out var a)),
+			(2, new DummyDisposable().Tee(out var b)),
+			(3, new DummyDisposable().Tee(out var c))
+		}
+			.Tee(out var list)
+			.RemoveAtAndTryDispose(1);
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed); // b is disposable, but the tuple is not!
+		Assert.IsFalse(c.IsDisposed);
+
+		Assert.AreEqual(new[] { (1, a), (3, c) }, list);
+	}
+
+	[Test]
+	public static void IList_T_SetAndTryDispose() {
+		new List<(int, DummyDisposable)> {
+			(1, new DummyDisposable().Tee(out var a)),
+			(2, new DummyDisposable().Tee(out var b)),
+			(3, new DummyDisposable().Tee(out var c))
+		}
+			.Tee(out var list)
+			.SetAndTryDispose(1, (4, new DummyDisposable().Tee(out var d)));
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed); // b is disposable, but the tuple is not!
+		Assert.IsFalse(c.IsDisposed);
+		Assert.IsFalse(d.IsDisposed);
+
+		Assert.AreEqual(new[] { (1, a), (4, d), (3, c) }, list);
+	}
+
+	// IDictionary<TKey, TValue>
+	// ----
+
+	[Test]
+	public static void IDictionary_T_RemoveAndTryDispose() {
+		new Dictionary<char, (int, DummyDisposable)> {
+			['A'] = (1, new DummyDisposable().Tee(out var a)),
+			['B'] = (2, new DummyDisposable().Tee(out var b)),
+			['C'] = (3, new DummyDisposable().Tee(out var c))
+		}
+			.Tee(out var dictionary)
+			.RemoveAndTryDispose('B');
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed); // b is disposable, but the tuple is not!
+		Assert.IsFalse(c.IsDisposed);
+
+		Assert.AreEqual(new[] { KeyValuePair.Create('A', (1, a)), KeyValuePair.Create('C', (3, c)) }, dictionary);
+	}
+
+	[Test]
+	public static void IDictionary_T_RemoveAndTryDispose_non_existent() {
+		new Dictionary<char, (int, DummyDisposable)>()
+			.Tee(out var dictionary)
+			.RemoveAndTryDispose('X');
+
+		Assert.IsEmpty(dictionary);
+	}
+
+	[Test]
+	public static void IDictionary_T_SetAndTryDispose() {
+		new Dictionary<char, (int, DummyDisposable)> {
+			['A'] = (1, new DummyDisposable().Tee(out var a)),
+			['B'] = (2, new DummyDisposable().Tee(out var b)),
+			['C'] = (3, new DummyDisposable().Tee(out var c))
+		}
+			.Tee(out var dictionary)
+			.SetAndTryDispose('B', (4, new DummyDisposable().Tee(out var d)));
+
+		Assert.IsFalse(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed); // b is disposable, but the tuple is not!
+		Assert.IsFalse(c.IsDisposed);
+		Assert.IsFalse(d.IsDisposed);
+
+		Assert.AreEqual(new[] { KeyValuePair.Create('A', (1, a)), KeyValuePair.Create('B', (4, d)), KeyValuePair.Create('C', (3, c)) }, dictionary);
+	}
+
+	[Test]
+	public static void IDictionary_T_SetAndDisTrypose_non_existent() {
+		new Dictionary<char, (int, DummyDisposable)>()
+			.Tee(out var dictionary)
+			.SetAndTryDispose('X', (0, new DummyDisposable().Tee(out var x)));
+
+		Assert.IsFalse(x.IsDisposed);
+
+		Assert.AreEqual(new[] { KeyValuePair.Create('X', (0, x)) }, dictionary);
+	}
+
+	// Legacy
+	// ----
 
 	[Test]
 	public static void DisposeAll_IEnumerable_eager() {
@@ -79,9 +472,10 @@ static class DisposableExtensionsTests {
 			('B', new DummyDisposable().Tee(out var b)),
 			('C', new DummyDisposable().Tee(out var c))
 		}
-			.Tee(out var list)
-			.DisposeAll(it => it.Item2)
-			.Clear();
+			.Tee(out var list);
+
+		list.DisposeAll(it => it.Item2);
+		list.Clear();
 
 		Assert.IsTrue(a.IsDisposed);
 		Assert.IsTrue(b.IsDisposed);
@@ -97,14 +491,73 @@ static class DisposableExtensionsTests {
 			['B'] = new DummyDisposable().Tee(out var b),
 			['C'] = new DummyDisposable().Tee(out var c)
 		}
-			.Tee(out var dictionary)
-			.DisposeAll(it => it.Value)
-			.Clear();
+			.Tee(out var dictionary);
+
+		dictionary.DisposeAll();
+		dictionary.Clear();
 
 		Assert.IsTrue(a.IsDisposed);
 		Assert.IsTrue(b.IsDisposed);
 		Assert.IsTrue(c.IsDisposed);
 
 		Assert.IsEmpty(dictionary);
+	}
+	
+	[Test]
+	public static void DisposeAll_IDictionary_selector() {
+		new Dictionary<char, DummyDisposable>(3) {
+			['A'] = new DummyDisposable().Tee(out var a),
+			['B'] = new DummyDisposable().Tee(out var b),
+			['C'] = new DummyDisposable().Tee(out var c)
+		}
+			.Tee(out var dictionary);
+
+		dictionary.DisposeAll(it => it.Value);
+		dictionary.Clear();
+
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+
+		Assert.IsEmpty(dictionary);
+	}
+
+	[Test]
+	public static void Set_Remove_Clear_AndDispose_IDictionary() {
+		var a = new DummyDisposable();
+		var b = new DummyDisposable();
+		var c = new DummyDisposable();
+		var d = new DummyDisposable();
+
+		var dictionary = new Dictionary<int, object> {
+			[1] = a,
+			[2] = b,
+			[3] = c
+		};
+
+		dictionary.SetAndTryDispose(1, d);
+
+		Assert.AreEqual(new[] { d, b, c }, dictionary.Values);
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsFalse(b.IsDisposed);
+		Assert.IsFalse(c.IsDisposed);
+		Assert.IsFalse(d.IsDisposed);
+
+		dictionary.RemoveAndTryDispose(2);
+
+		Assert.AreEqual(new[] { d, c }, dictionary.Values);
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsFalse(c.IsDisposed);
+		Assert.IsFalse(d.IsDisposed);
+
+		dictionary.TryDisposeAll();
+		dictionary.Clear();
+
+		Assert.IsEmpty(dictionary);
+		Assert.IsTrue(a.IsDisposed);
+		Assert.IsTrue(b.IsDisposed);
+		Assert.IsTrue(c.IsDisposed);
+		Assert.IsTrue(d.IsDisposed);
 	}
 }
--- LinqToYourDoom/System/Extensions/DisposableExtensions.cs
+++ LinqToYourDoom/System/Extensions/DisposableExtensions.cs
@@ -4,44 +4,274 @@ using System.Collections.Generic;
 namespace LinqToYourDoom;
 
 public static class DisposableExtensions {
+	// IEnumerable<T> where T : IDisposable
+	// ----
+
 	/// <summary>
 	/// Disposes all items in <paramref name="this"/> sequence.
 	/// </summary>
 	///
 	/// <remarks>
-	/// <see cref="DisposeAll{T}(T)"/> is <b>eager</b>.
+	/// <see cref="DisposeAll{T}(IEnumerable{T})"/> is <b>eager</b>.
 	/// For a <b>lazy</b> equivalent, use <c>@this.<see cref="EnumerableExtensions.Each{T}(IEnumerable{T}, Action{T})">Each</see>(it => it.Dispose())</c>.
 	/// </remarks>
-	///
-	/// <returns> <paramref name="this"/>. </returns>
-	public static T DisposeAll<T>(this T @this) where T : IEnumerable<IDisposable> {
+	public static void DisposeAll<T>(this IEnumerable<T> @this) where T : IDisposable {
 		foreach (var disposable in @this)
 			disposable.Dispose();
-
-		return @this;
 	}
 
-	/// <inheritdoc cref="DisposeAll{T}(T)"/>
-	public static IEnumerable<T> DisposeAll<T, TDisposable>(this IEnumerable<T> @this, Func<T, TDisposable> selector) where TDisposable : IDisposable {
-		foreach (var item in @this)
-			selector.Invoke(item).Dispose();
-
-		return @this;
+	/// <summary>
+	/// Disposes all items in <paramref name="this"/> sequence that satisfy the <paramref name="predicate"/>.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T})"/>
+	public static void DisposeAll<T>(this IEnumerable<T> @this, Predicate<T> predicate) where T : IDisposable {
+		foreach (var disposable in @this)
+			if (predicate.Invoke(disposable))
+				disposable.Dispose();
 	}
 
-	/// <inheritdoc cref="DisposeAll{T}(T)"/>
-	public static ICollection<T> DisposeAll<T, TDisposable>(this ICollection<T> @this, Func<T, TDisposable> selector) where TDisposable : IDisposable {
-		foreach (var item in @this)
-			selector.Invoke(item).Dispose();
+	// IReadOnlyDictionary<TKey, TValue> where TValue : IDisposable
+	// ----
 
-		return @this;
+	/// <summary>
+	/// Disposes all values in <paramref name="this"/> dictionary.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T})"/>
+	public static void DisposeAll<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this) where TValue : IDisposable {
+		foreach (var (_, disposable) in @this)
+			disposable.Dispose();
 	}
 
-	/// <inheritdoc cref="DisposeAll{T}(T)"/>
-	public static IDictionary<TKey, TValue> DisposeAll<TKey, TValue, TDisposable>(this IDictionary<TKey, TValue> @this, Func<KeyValuePair<TKey, TValue>, TDisposable> selector) where TDisposable : IDisposable {
+	/// <summary>
+	/// Disposes all values in <paramref name="this"/> dictionary that satisfy the <paramref name="predicate"/>.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{TKey, TValue}(IReadOnlyDictionary{TKey, TValue})"/>
+	public static void DisposeAll<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this, Predicate<TValue> predicate) where TValue : IDisposable {
+		foreach (var (_, disposable) in @this)
+			if (predicate.Invoke(disposable))
+				disposable.Dispose();
+	}
+	
+	/// <inheritdoc cref="DisposeAll{TKey, TValue}(IReadOnlyDictionary{TKey, TValue}, Predicate{TValue})"/>
+	public static void DisposeAll<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this, Func<TKey, TValue, bool> predicate) where TValue : IDisposable {
+		foreach (var (key, disposable) in @this)
+			if (predicate.Invoke(key, disposable))
+				disposable.Dispose();
+	}
+
+	// IEnumerable<T>
+	// ----
+
+	/// <summary>
+	/// Disposes all items that implement <see cref="IDisposable"/> in <paramref name="this"/> sequence.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T})"/>
+	public static void TryDisposeAll<T>(this IEnumerable<T> @this) {
+		foreach (var item in @this)
+			if (item is IDisposable disposable)
+				disposable.Dispose();
+	}
+
+	/// <summary>
+	/// Disposes all disposables returned by the <paramref name="selector"/> from the items in <paramref name="this"/> sequence.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T})"/>
+	public static void DisposeAll<T>(this IEnumerable<T> @this, Func<T, IDisposable> selector) {
 		foreach (var item in @this)
 			selector.Invoke(item).Dispose();
+	}
 
-		return @this;
+	/// <summary>
+	/// Disposes all disposables returned by the <paramref name="trySelector"/> from the items in <paramref name="this"/> sequence.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T}, Func{T, IDisposable})"/>
+	public static void DisposeAll<T>(this IEnumerable<T> @this, TryFunc<T, IDisposable> trySelector) {
+		foreach (var item in @this)
+			if (trySelector.Invoke(item, out var disposable))
+				disposable.Dispose();
+	}
+
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T}, TryFunc{T, IDisposable})"/>
+	public static void DisposeAll<T>(this IEnumerable<T> @this, Func<T, (bool, IDisposable)> trySelector) {
+		foreach (var item in @this) {
+			var (isDisposable, disposable) = trySelector.Invoke(item);
+
+			if (isDisposable) disposable.Dispose();
+		}
+	}
+
+
+	// IReadOnlyDictionary<TKey, TValue>
+	// ----
+
+	/// <summary>
+	/// Disposes all values that implement <see cref="IDisposable"/> in <paramref name="this"/> dictionary.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T})"/>
+	public static void TryDisposeAll<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this) {
+		foreach (var (_, value) in @this)
+			if (value is IDisposable disposable)
+				disposable.Dispose();
+	}
+
+	/// <summary>
+	/// Disposes all disposables returned by the <paramref name="selector"/> from the values in <paramref name="this"/> dictionary.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{T}(IEnumerable{T})"/>
+	public static void DisposeAll<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this, Func<TKey, TValue, IDisposable> selector) {
+		foreach (var (key, value) in @this)
+			selector.Invoke(key, value).Dispose();
+	}
+
+	/// <summary>
+	/// Disposes all disposables returned by the <paramref name="trySelector"/> from the values in <paramref name="this"/> dictionary.
+	/// </summary>
+	///
+	/// <inheritdoc cref="DisposeAll{TKey, TValue}(IReadOnlyDictionary{TKey, TValue}, Func{TKey, TValue, IDisposable})"/>
+	public static void DisposeAll<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this, TryFunc<TKey, TValue, IDisposable> trySelector) {
+		foreach (var (key, value) in @this)
+			if (trySelector.Invoke(key, value, out var disposable))
+				disposable.Dispose();
+	}
+
+	/// <inheritdoc cref="DisposeAll{TKey, TValue}(IReadOnlyDictionary{TKey, TValue}, TryFunc{TKey, TValue, IDisposable})"/>
+	public static void DisposeAll<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> @this, Func<TKey, TValue, (bool, IDisposable)> trySelector) {
+		foreach (var (key, value) in @this) {
+			var (isDisposable, disposable) = trySelector.Invoke(key, value);
+
+			if (isDisposable) disposable.Dispose();
+		}
+	}
+
+	// ICollection<T> where T : IDisposable
+	// ----
+
+	/// <summary>
+	/// Removes an item from this collection, also disposing the item.
+	/// </summary>
+	public static void RemoveAndDispose<T>(this ICollection<T> @this, T item) where T : IDisposable {
+		@this.Remove(item);
+		item.Dispose();
+	}
+
+	// IList<T> where T : IDisposable
+	// ----
+
+	/// <summary>
+	/// Removes the item at the specified <paramref name="index"/> from this list,
+	/// also disposing the item.
+	/// </summary>
+	public static void RemoveAtAndDispose<T>(this IList<T> @this, int index) where T : IDisposable {
+		var disposable = @this[index];
+
+		@this.RemoveAt(index);
+		disposable.Dispose();
+	}
+
+	/// <summary>
+	/// Overrides the value at the specifed <paramref name="index"/>,
+	/// also disposing the existing value.
+	/// </summary>
+	public static void SetAndDispose<T>(this IList<T> @this, int index, T value) where T : IDisposable {
+		@this[index].Tee(out var disposable);
+		@this[index] = value;
+
+		disposable.Dispose();
+	}
+
+	// IDictionary<TKey, TValue> where TValue : IDisposable
+	// ----
+
+	/// <summary>
+	/// Removes the value for the specified <paramref name="key"/> from this dictionary,
+	/// also disposing the value.
+	/// </summary>
+	public static void RemoveAndDispose<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key) where TValue : IDisposable {
+		if (@this.Remove(key, out var disposable))
+			disposable.Dispose();
+	}
+
+	/// <summary>
+	/// Overrides the value associated with a <paramref name="key"/>,
+	/// also disposing any existing value.
+	/// </summary>
+	public static void SetAndDispose<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue value) where TValue : IDisposable {
+		if (@this.Remove(key, out var disposable))
+			disposable.Dispose();
+
+		@this[key] = value;
+	}
+
+	// ICollection<T>
+	// ----
+
+	/// <summary>
+	/// Removes an item from this collection,
+	/// also disposing it if it implements <see cref="IDisposable"/>.
+	/// </summary>
+	public static void RemoveAndTryDispose<T>(this ICollection<T> @this, T item) {
+		@this.Remove(item);
+		
+		if (item is IDisposable disposable)
+			disposable.Dispose();
+	}
+
+	// IList<T>
+	// ----
+
+	/// <summary>
+	/// Removes the item at the specified <paramref name="index"/> from this list,
+	/// also disposing it if it implements <see cref="IDisposable"/>.
+	/// </summary>
+	public static void RemoveAtAndTryDispose<T>(this IList<T> @this, int index) {
+		var item = @this[index];
+
+		@this.RemoveAt(index);
+
+		if (item is IDisposable disposable)
+			disposable.Dispose();
+	}
+
+	/// <summary>
+	/// Overrides the value at the specifed <paramref name="index"/>,
+	/// also disposing the existing value if it implements <see cref="IDisposable"/>.
+	/// </summary>
+	public static void SetAndTryDispose<T>(this IList<T> @this, int index, T value) {
+		@this[index].Tee(out var item);
+		@this[index] = value;
+
+		if (item is IDisposable disposable)
+			disposable.Dispose();
+	}
+
+	// IDictionary<TKey, TValue>
+	// ----
+
+	/// <summary>
+	/// Removes the value for the specified <paramref name="key"/> from this dictionary,
+	/// also disposing it if it implements <see cref="IDisposable"/>.
+	/// </summary>
+	public static void RemoveAndTryDispose<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key) {
+		if (@this.Remove(key, out var item) && item is IDisposable disposable)
+			disposable.Dispose();
+	}
+
+	/// <summary>
+	/// Overrides the value associated with a <paramref name="key"/>,
+	/// also disposing any existing value if it implements <see cref="IDisposable"/>.
+	/// </summary>
+	public static void SetAndTryDispose<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key, TValue value) {
+		if (@this.Remove(key, out var item) && item is IDisposable disposable)
+			disposable.Dispose();
+
+		@this[key] = value;
 	}
 }
